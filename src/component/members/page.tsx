722eea69ed24ce8313e2cc6e6e1973e8b4b29678
JungHoon Park <wfr112@naver.com>
2019-01-19 16:28:14 +0900

    Complete 04

--- src/component/members/page.tsx
+++ src/component/members/page.tsx
@@ -1,9 +1,55 @@
-import * as React from 'react'
+import * as React from "react";
+import { memberAPI } from "../../api/member";
 
-export const MembersPage: React.SFC<{}> = () => {
+interface State {
+  members: MemberEntity[];
+}
+interface Props {}
+
+export class MembersPage extends React.Component<Props, State> {
+  state = { members: [] };
+
+  public componentDidMount() {
+    memberAPI.fetchMembers().then(members => {
+      this.setState({ members });
+    });
+  }
+
+  public render() {
     return (
-        <div className='row'>
-            <h2> Members Page </h2>
-        </div>
-    )
+      <div className="row">
+        <h2> Members Page </h2>
+        <table className="table">
+          <thead>{this.MemberHeader()}</thead>
+          <tbody>{this.state.members.map(this.MemberRow)}</tbody>
+        </table>
+      </div>
+    );
+  }
+
+  private MemberHeader = () => {
+    return (
+      <tr>
+        <th>Avatar</th>
+        <th>Id</th>
+        <th>Name</th>
+      </tr>
+    );
+  };
+
+  private MemberRow = (member: MemberEntity) => {
+    return (
+      <tr key={member.id}>
+        <td>
+          <img src={member.avatar_url} className="avatar" />
+        </td>
+        <td>
+          <span>{member.id}</span>
+        </td>
+        <td>
+          <span>{member.login}</span>
+        </td>
+      </tr>
+    );
+  };
 }
---
+++ src/model/memberEntity.ts
@@ -0,0 +1,5 @@
+declare interface MemberEntity {
+    id: number
+    login: string
+    avatar_url: string
+}
