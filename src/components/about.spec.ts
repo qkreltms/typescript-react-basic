4efd6e729534fac13e0283056e8075579601e39f
JungHoon Park <wfr112@naver.com>
2019-01-26 15:15:23 +0900

    On practice

---
+++ src/components/about.spec.ts
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { About } from "./about";
+import { shallow } from "enzyme";
+
+describe("components/about tests", () => {
+  it("should render as expected ", () => {
+    //Arrange
+    //Act
+    const component = shallow(<About />);
+    //Assert
+    expect(component).toMatchSnapshot();
+  });
+});
--- src/components/about.tsx
+++ src/components/about.tsx
@@ -55,5 +55,5 @@ export const About: React.SFC<{}> = () => {
         </ul>
       </div>
     </div>
-    )
+    ) 
 }
